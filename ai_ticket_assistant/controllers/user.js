e1ba079efe9ad8297e37985c5957331fe3bb9b21
Shiva Marath <shivamarath2005@gmail.com>
2025-09-28 18:31:48 +0530

    added simple auth

--- ai_ticket_assistant/controllers/user.js
+++ ai_ticket_assistant/controllers/user.js
@@ -1,4 +1,21 @@
-import bcrpt from "bcrypt"
+import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-import user from "../models/user.js"
+import User from "../models/user.js"
 import { inngest } from "../inngest/client.js"
+
+export const signup = async (req, res)=>{
+    const {email, password, skills} = req.body
+    try {
+        const hashed = bcrypt.hash(password, 10)
+        const user = await User.create({email, password: hashed, skills})
+        //inngest
+        await inngest.send({name: "user/signup",
+            data:{
+            email
+        }})
+
+        const token = jwt.sign({_id: user._id , role: user.role} , process.env.JWT_SECRET)
+    } catch (error) {
+        
+    }
+}
