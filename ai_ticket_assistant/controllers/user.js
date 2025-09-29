373daf7f462e43a0bf0ca2c5f7a119da1579d45f
Shiva Marath <shivamarath2005@gmail.com>
2025-09-30 04:37:11 +0530

    added simple auth

--- ai_ticket_assistant/controllers/user.js
+++ ai_ticket_assistant/controllers/user.js
@@ -18,6 +18,42 @@ export const signup = async (req, res)=>{
 
         res.json({user, token})
     } catch (error) {
-        
+        res.status(500).json({error: "Signup failed", details: error.message})
     }
+}
+
+export const signin = async (req, res)=>{
+    const {email, password} = req.body
+    try {
+        const User = User.findOne({email})
+        if(!User){
+            res.status(403).json({msg:"Unauthorized"})
+        }
+        const isMatched = await bcrypt.compare(password, User.password)
+        if(!isMatched){
+            return res.status(403).json({Unauthorized})
+        }
+        const token = jwt.sign({_id: user._id , role: user.role} , process.env.JWT_SECRET)
+        res.json({User, token})
+    } catch (error) {
+         res.status(500).json({error: "Signin failed", details: error.message})
+    }
+}
+
+export const logout  = async (req,res)=>{
+    try {
+         const token = req.headers.authorization.split(" ")[1]
+         if(!token){
+            return res.json({msg: "Unauthorized"})
+            jwt.verify(token, process.env.JWT_SECRET, (err,decoded)=>{
+                if(err){
+                    return res.json({msg: "Unauthorized"})
+                }
+            })
+         }
+    res.json({msg: "logged out"})
+    } catch (error) {
+         res.status(500).json({error: "Logout failed", details: error.message})
+    }
+   
 }
